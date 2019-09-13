6db12d6cbb7b2cd7c527ac74d80790661c0cbccc
SudheerReddySingam <54614069+SudheerReddySingam@users.noreply.github.com>
2019-09-13 17:08:48 +0530

    updated quiz page

---
+++ app.js
@@ -0,0 +1,43 @@
+// $(document).ready(function(){
+//     if(typeof(storage)!=="undefined")
+//     {
+//         $getJSON('ques-db.json',function(data){
+//             selectedQue=data[Math.floor(Math.random()*quotes.length)]
+//         })
+//     }
+// })
+$(function(){
+    // data in attributes
+    // getAttributeData()
+    getJSON();
+    // getLocalData()
+ })
+ console.log("sudheer reddy");
+ function getJSON(){
+    $.getJSON('ques-db.json',function(json){
+        data = json;
+        console.log(typeof data);
+        console.log(data);
+        // localStorage.setItem("data",JSON.stringify(data))
+        // let varr=localStorage.getItem("data");
+        // let obj=JSON.parse(varr)
+
+
+        const keys=Object.keys(data);
+        console.log(typeof keys);
+        let getQuestionAndAnswer = data[keys[3]];
+        console.log(getQuestionAndAnswer);
+        let getQuestion=getQuestionAndAnswer["question"];
+        document.getElementById("que").innerHTML=getQuestion;
+        let getOptions=getQuestionAndAnswer["options"];
+        let option1=getOptions[0];
+        document.getElementsByClassName("opt1")[0].innerHTML=option1;
+        let option2=getOptions[1];
+        document.getElementsByClassName("opt2")[0].innerHTML=option2;
+        console.log(option2);
+        let option3=getOptions[2];
+        document.getElementsByClassName("opt3")[0].innerHTML=option3;
+        let option4=getOptions[3];
+        document.getElementsByClassName("opt4")[0].innerHTML=option4;
+    })
+}
