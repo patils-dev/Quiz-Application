8113098ccaff0b6ddd25028a46ce113ef66c1dde
krushnap <smitakpatil26@gmail.com>
2019-09-27 17:06:05 +0530

    updated quiz

--- javascript/app.js
+++ javascript/app.js
@@ -1,14 +1,30 @@
 let selectedIndex;
 let text;
-let quesIndex=0;
+let queIndex=0;
 let arr;
 let ansArr=[];
 let queCount=0;
 let result;
 
 
+function getQues(){
+    queCount++;
+
+    $(".questionNumber").text(queCount+"/ 10")
+    // console.log(arr);
+    if(queCount==10)
+    {
+         $(".submitButton").css("display", "flex");
+    }
+    $(".question").text(result[0].questions[queIndex].question);
+    $(".option1").text(result[0].questions[queIndex].options[0]);
+    $(".option2").text(result[0].questions[queIndex].options[1]);
+    $(".option3").text(result[0].questions[queIndex].options[2]);
+    $(".option4").text(result[0].questions[queIndex].options[3]);
+    queIndex++;
+}
+
 $(document).ready(function(){
-    
 
     $.ajax({
         type: "GET",
@@ -16,30 +32,22 @@ $(document).ready(function(){
         url: "http://localhost:8000/",
         success: function(data){
            result=data;
+           console.log(result)
+        //    console.log(result[0].questions[0].question)
         },
         error:function(err){
             console.log(err);
         }
     });
-
     getQues();
+
+   
 })
 
 // ['q1':0,'q4':5]
 
 
-function getQues(){
-        queCount++;
 
-        $(".questionNumber").text(queCount+"/ 10")
-        // console.log(arr);
-        $(".question").text(result[0].questions[queCount].question);
-        $(".option1").text(result[0].questions[queCount].options[0]);
-        $(".option2").text(result[0].questions[queCount].options[1]);
-        $(".option3").text(result[0].questions[queCount].options[2]);
-        $(".option4").text(result[0].questions[queCount].options[3]);
-
-}
 $(".next").click(function(){
     storeAns(selectedIndex,text)
     if(queCount<10)
@@ -47,10 +55,7 @@ $(".next").click(function(){
         getQues();
         $(".option1,.option2,.option3,.option4").css("background-color", "#E0F2F1");
     }
-    if(queCount==10)
-    {
-        $(".submitButton").css("display", "flex");
-    }
+    
 
 });
 
