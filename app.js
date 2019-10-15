3c184ae7556166f5c74df36142cfc9753cb97e40
krushnap <smitakpatil26@gmail.com>
2019-10-15 17:22:13 +0530

    changes in js and html files

--- app.js
+++ app.js
@@ -1,77 +1,97 @@
 $(document).ready(function(){
-    getJSON1();
+  getJSON1();
 })
 let selectedIndex;
 let keys=" ";
 let text;
-function getJSON1(){
-  let queCount1=
-  $.getJSON('ques-db.json',function(json){
-      keys=Object.values(json)
-      
-      let randIndex =Math.floor(Math.round(Math.random()*(keys.length)));
-      console.log(randIndex);
-      selectedIndex=randIndex;
+let quesIndex=0;
+let arr;
+let ansArr=[];
+let queCount=0;
 
-        $(".question").text(keys[randIndex].question);
-        $(".option1").text(keys[randIndex].options[0]);
-        $(".option2").text(keys[randIndex].options[1]);
-        $(".option3").text(keys[randIndex].options[2]);
-        $(".option4").text(keys[randIndex].options[3]);
-})
+
+$.getJSON('ques-db.json',function(json){
+  keys=Object.values(json)
+    arr=keys;})
+
+function getJSON1(){
+      queCount++;
+      $(".questionNumber").text(queCount+"/ 10")
+      console.log(arr);
+      $(".question").text(arr[quesIndex].question);
+      $(".option1").text(arr[quesIndex].options[0]);
+      $(".option2").text(arr[quesIndex].options[1]);
+      $(".option3").text(arr[quesIndex].options[2]);
+      $(".option4").text(arr[quesIndex].options[3]);
+      quesIndex++;
 }
 
 $('div.option1').click(function(){
-  console.log("div1")
-    text = $(this).index();
-    console.log(text)
+  // text = $(this).index();
+  text = $(this).text();
+  console.log(text)
 })
- $(".next").click(function(){
-  console.log("checkans")
-  checkAns(selectedIndex,text);
-  getJSON1();
+
+$(".next").click(function(){
+      storeAns(selectedIndex,text)
+      if(queCount<10)
+      {
+          getJSON1();
+          $(".option1,.option2,.option3,.option4").css("background-color", "#E0F2F1");
+      }
+      if(queCount==10)
+      {
+          $(".submitButton").css("display", "flex");
+      }
+
 });
 $('div.option2').click(function() {
-  console.log("div2")
-   text = $(this).text();
-    // $(".next").click(function(){
-    //   console.log("checkans")
-    // checkAns(selectedIndex,text);
-    // getJSON1(); 
-  // });
+
+  //  text = $(this).index();
+  text = $(this).text();
+ console.log(text)
 })
 
 $('div.option3').click(function() {
-  console.log("div3")
-    let text = $(this).text();
-  //   $(".next").click(function(){
-  //     console.log("checkans")
-  //   checkAns(selectedIndex,text);
-  //   getJSON1(); 
-  // });
+
+  //  text = $(this).index();
+  text = $(this).text();
+  console.log(text)
 })
 
 $('div.option4').click(function() {
-  console.log("div4")
-    let text = $(this).text();
-  //   $(".next").click(function(){
-  //     console.log("checkans")
-  //    checkAns(selectedIndex,text);
-  //   getJSON1(); 
-  // });
+
+  //  text = $(this).index();
+  text = $(this).text();
+  console.log(text)
+})
+
+$(".submitButton").click(function(){
+  checkAns();
+  // console.log("checkans")
 })
 
 
-let queCount=0;
-let correctAns=0
-function checkAns(index,text)
+function storeAns(index,text)
 {
-  console.log("checkans")
-    if(text==keys[index].answer)
-    {
-      correctAns++;
-    }
-    queCount++;
-    console.log(correctAns)
-    console.log(queCount)
+  ansArr.push(text);
+  console.log(ansArr);
+}
+
+function checkAns()
+{
+  let correctAns=0;
+  for(let i=0;i<ansArr.length;i++)
+  {
+      console.log(ansArr[i])
+      console.log(arr[i].answer)
+      if(ansArr[i]==arr[i].answer)
+      {
+          correctAns++;
+      }
+  }
+  // console.log(correctAns);
+  alert(`You Scored : ${correctAns}`);
+  // document.getElementById('scored').innerText=correctAns;
+  // $(".score").text(correctAns+"/ 10")
 }
